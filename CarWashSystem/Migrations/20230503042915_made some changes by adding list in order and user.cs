925fa565609114c5ff4145853c424095509f906e
Mahajan <abhishek-shriram.mahajan@capgemini.com>
2023-05-09 10:45:40 +0530

    first commit

---
+++ CarWashSystem/Migrations/20230503042915_made some changes by adding list in order and user.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarWashSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class madesomechangesbyaddinglistinorderanduser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Orders_OrderId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_OrderId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Cars");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CarId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CarId",
+                table: "Orders",
+                column: "CarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CarId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CarId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Payments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Cars",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_OrderId",
+                table: "Cars",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Orders_OrderId",
+                table: "Cars",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
